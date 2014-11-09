c3e3b8b35898c06fe41af9d48780050a3d0a8c13
TrippW <trwlll@mindspring.com>
2014-11-09 07:51:31 -0600

    Strip pixel from level

--- PhysGame/src/physgame/Level.java
+++ PhysGame/src/physgame/Level.java
@@ -1,5 +1,10 @@
 package physgame;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
 class Level {
     
     public Sprite goal = Sprite.goal;
@@ -8,12 +13,19 @@ class Level {
     private GameObject[] objs = new GameObject[2];
     private boolean isWon = false;
     private boolean isStart = false;
+    private BufferedImage bi;
     
     
     public Level(String p, Screen screen){
-        path = p;
-        objs[0] = new GameObject(30, 50, 0, 0, 25, true, screen);
-        objs[1] = new GameObject(180, 50, 0, 0, 25, true, screen);
+        try {
+            path = p;
+            objs[0] = new GameObject(30, 50, 0, 0, 25, true, screen);
+            objs[1] = new GameObject(180, 50, 0, 0, 25, true, screen);
+            bi = ImageIO.read(getClass().getResourceAsStream(path));
+            bi.getRGB(0, 0, 250, 300, levelLayout, 0, 250);
+        } catch (IOException ex) {
+            Logger.getLogger(Level.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
     }
     
     public void render(){
